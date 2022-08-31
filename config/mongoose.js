33c532c932d9cf91b4120dcac3c173b6eb54d740
nikuwadaskar <101786620+nikuwadaskar@users.noreply.github.com>
2022-08-31 21:39:07 +0530

    working

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/codeial_development");
 
-const db=mongoose.connection
+const db = mongoose.connection;
 
-db.on("error",console.error(condole,"error in conncting"))
+db.on("error", console.error.bind(console, "error in conncting"));
 
-db.once('open', function(){
-    console.log("connected to database");
-})
- module.exports=db
+db.once("open", function () {
+  console.log("connected to database");
+});
+module.exports = db;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-const db=require("./config/mongoose")
+const db = require("./config/mongoose");
 const expressLatout = require("express-ejs-layouts");
 
 app.use(express.static("./assets"));
